f477e51d6059523524502d39624f82bb83ccf22c
Rahul <rahulkumar71993@gmail.com>
2019-02-16 01:30:19 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: "Second Flutter App",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Second App"),backgroundColor: Colors.green,),
+        body: Material(
+          color: Colors.lightGreen,
+          child: Center(
+            child: Text("Hello Second Dart", style:TextStyle(color: Colors.black ,fontSize: 30.00),),
+          ),
+        ),
+      ),
+    );
+  }
+}
